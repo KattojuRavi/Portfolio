449e64ea167976266e115e68a89e5f2bbae649bb
KattojuRavi <101752132+KattojuRavi@users.noreply.github.com>
2025-04-12 02:06:16 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+{\rtf1\ansi\ansicpg1252\cocoartf2636
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
+{\colortbl;\red255\green255\blue255;\red255\green255\blue255;}
+{\*\expandedcolortbl;;\cssrgb\c100000\c100000\c100000;}
+\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+\deftab720
+\pard\pardeftab720\partightenfactor0
+
+\f0\fs24 \cf0 \expnd0\expndtw0\kerning0
+document.getElementById("contact-form").addEventListener("submit", function(event) \{\
+   event.preventDefault();\
+   alert("Thank you for reaching out!");\
+\});}
